5cc6eba756b6898d4968c1ee2c4fdd8b6e11be3d
2406Arsen <polkpolk356@gmail.com>
2021-07-03 17:39:16 +0400

    as

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import reactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
 
 
